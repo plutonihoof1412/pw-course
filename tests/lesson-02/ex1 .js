e716505bd6755b3d4257c42474ff877fc621d0f8
Toni Ho <85222482+plutonihoof1412@users.noreply.github.com>
2026-07-18 12:28:18 +0700

    Implement variable declarations and even check
    Added variable declarations and even number check.

--- tests/lesson-02/ex1 .js
+++ tests/lesson-02/ex1 .js
@@ -1,4 +1,14 @@
 a. Khai báo một hằng số number với giá trị là 12. 
+  -> const a = 12;
+
 b. Khai báo một biến name với giá trị là "my number" 
+  -> let name = "my number";
+
 c. Khai báo một biến isEven với giá trị là sai. 
+  -> let isEvent = false;
+
 d. Kiểm tra nếu number là số chẵn, gán lại isEven thành giá trị đúng.
+  -> let x = number;
+     if (x % 2 === 0) {
+      isEvent = true };
+  
