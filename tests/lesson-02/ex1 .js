3294037558436171c191c5355fac7df8649ca967
Toni Ho <85222482+plutonihoof1412@users.noreply.github.com>
2026-07-18 12:05:02 +0700

    Create ex1 .js
    not finished yet

---
+++ tests/lesson-02/ex1 .js
@@ -0,0 +1,4 @@
+a. Khai báo một hằng số number với giá trị là 12. 
+b. Khai báo một biến name với giá trị là "my number" 
+c. Khai báo một biến isEven với giá trị là sai. 
+d. Kiểm tra nếu number là số chẵn, gán lại isEven thành giá trị đúng.
