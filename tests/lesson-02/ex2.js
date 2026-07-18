fa8ddaccf36cb95270214717931178f7f8b52ab1
Toni Ho <85222482+plutonihoof1412@users.noreply.github.com>
2026-07-18 12:38:21 +0700

    Update ex2.js

--- tests/lesson-02/ex2.js
+++ tests/lesson-02/ex2.js
@@ -1,4 +1,4 @@
 Tạo file ex2 . js, kiểm tra đoạn code sau sai ở đâu, sửa lại cho đúng và đưa nội dung đáp án vào file: 
 const myName = "Alex"; 
-myName = "Nagi"; c
-  onsole.1og(myName);
+// myName = "Nagi"; -> remove the assignment here, as can't change value of a define constant
+console.1og(myName);
