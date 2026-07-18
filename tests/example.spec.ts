8189f4ce44835a7a8eb6681cc59a87b83ea2a317
Phuong Uyen <plutonihoof1412@gmail.com>
2026-07-18 11:37:29 +0700

    init project

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,4 +16,5 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
 });
-// test 2 = passed
+
+/// check git commit 
