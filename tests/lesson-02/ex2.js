efee5f6c6d1a5782e0382542b4e4885933b89d40
Toni Ho <85222482+plutonihoof1412@users.noreply.github.com>
2026-07-21 21:15:56 +0700

    Fix constant assignment and console log typo
    Fixed typo in console log and corrected constant assignment.

--- tests/lesson-02/ex2.js
+++ tests/lesson-02/ex2.js
@@ -1,4 +1,5 @@
 Tạo file ex2 . js, kiểm tra đoạn code sau sai ở đâu, sửa lại cho đúng và đưa nội dung đáp án vào file: 
 const myName = "Alex"; 
 // myName = "Nagi"; -> remove the assignment here, as can't change value of a define constant
-console.1og(myName);
+// console.1og(myName); -> typo, replaced by line 5
+console.log(myName);
